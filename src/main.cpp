b0efecbd2eb5223429e7b702834337ea5df44037
Kuba <jakub.kuba.novak@gmail.com>
2022-01-07 16:01:36 +0100

    zmena v kodu

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,20 @@
-#include <Arduino.h>
-#ifdef ESP32
-  #include <WiFi.h>
-#else
-  #include <ESP8266WiFi.h>
-#endif
+#include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <UniversalTelegramBot.h> // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 #include <ArduinoJson.h>
-
+#define Hall_sensor 35
+int Val1 = 0;
 // Replace with your network credentials
-const char* ssid = "NOVACI_WIFI";
-const char* password = "K@TK@&F1L1P&KUB1K";
+const char *ssid = "NOVACI_WIFI";
+const char *password = "K@TK@&F1L1P&KUB1K";
 
 // Initialize Telegram BOT
-#define BOTtoken "2107324741:AAELdq0KJMGP6mD7PMTysN3Lx1Afgvswpks"  // your Bot Token (Get from Botfather)
+#define BOTtoken "2107324741:AAELdq0KJMGP6mD7PMTysN3Lx1Afgvswpks" // your Bot Token (Get from Botfather)
 
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
 #define CHAT_ID "2110767400"
-
-#ifdef ESP8266
-  X509List cert(TELEGRAM_CERTIFICATE_ROOT);
-#endif
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
@@ -35,25 +27,29 @@ const int ledPin = 2;
 bool ledState = LOW;
 
 // Handle what happens when you receive new messages
-void handleNewMessages(int numNewMessages) {
+void handleNewMessages(int numNewMessages)
+{
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
 
-  for (int i=0; i<numNewMessages; i++) {
+  for (int i = 0; i < numNewMessages; i++)
+  {
     // Chat id of the requester
     String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
+    if (chat_id != CHAT_ID)
+    {
       bot.sendMessage(chat_id, "Unauthorized user", "");
       continue;
     }
-    
+
     // Print the received message
     String text = bot.messages[i].text;
     Serial.println(text);
 
     String from_name = bot.messages[i].from_name;
 
-    if (text == "/start") {
+    if (text == "/start")
+    {
       String welcome = "Welcome, " + from_name + ".\n";
       welcome += "Use the following commands to control your outputs.\n\n";
       welcome += "/led_on to turn GPIO ON \n";
@@ -62,63 +58,107 @@ void handleNewMessages(int numNewMessages) {
       bot.sendMessage(chat_id, welcome, "");
     }
 
-    if (text == "/led_on") {
+    if (text == "/led_on")
+    {
       bot.sendMessage(chat_id, "LED state set to ON", "");
       ledState = HIGH;
       digitalWrite(ledPin, ledState);
     }
-    
-    if (text == "/led_off") {
+
+    if (text == "/led_off")
+    {
       bot.sendMessage(chat_id, "LED state set to OFF", "");
       ledState = LOW;
       digitalWrite(ledPin, ledState);
     }
-    
-    if (text == "/state") {
-      if (digitalRead(ledPin)){
+
+    if (text == "/state")
+    {
+      if (digitalRead(ledPin))
+      {
         bot.sendMessage(chat_id, "LED is ON", "");
       }
-      else{
+      else
+      {
         bot.sendMessage(chat_id, "LED is OFF", "");
       }
     }
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-
-  #ifdef ESP8266
-    configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
-    client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
-  #endif
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, ledState);
-  
+
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  #ifdef ESP32
-    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  #endif
-  while (WiFi.status() != WL_CONNECTED) {
+#ifdef ESP32
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+#endif
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
+
+  pinMode(Hall_sensor, INPUT);
+  /*while(Val1=2)
+  {
+    Val1 = digitalRead(Hall_sensor); //We read both values and display them raw on the serial monitor
+    Serial.print(Val1);
+    Serial.print("\t");
+  }
+  delay (1000);*/
 }
 
-void loop() {
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+/*void Hall_Sensor()
+{
+  Val1 = analogRead(Hall_sensor); //We read both values and display them raw on the serial monitor
+  Serial.print(Val1);
+  Serial.print("\t");
+  delay (1000);
+}*/
 
-    while(numNewMessages) {
+void loop()
+{
+  if (millis() > lastTimeBotRan + botRequestDelay)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+   
+    while (numNewMessages)
+    {
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
     lastTimeBotRan = millis();
   }
+  Val1=digitalRead(Hall_sensor);            //We read both values and display them raw on the serial monitor
+   Serial.print(Val1);
+   Serial.print("\t");
+   delay(1000);
 }
+#include <ArduinoJson.h>
+#define LED 34
+
+
+/*void setup () {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  
+}
+
+void loop () {
+  
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+  
+}*/
