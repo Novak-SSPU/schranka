64566fa7b8b3c9e4c58c96614941799fc97dc1f2
Kuba <jakub.kuba.novak@gmail.com>
2022-01-07 23:17:23 +0100

    Změny v kódu

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,30 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h> // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <UniversalTelegramBot.h> // Knihovna pro Telegram bota od Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 #include <ArduinoJson.h>
 #define Hall_sensor 35
 int Val1 = 0;
-// Replace with your network credentials
-const char *ssid = "NOVACI_WIFI";
-const char *password = "K@TK@&F1L1P&KUB1K";
 
-// Initialize Telegram BOT
-#define BOTtoken "2107324741:AAELdq0KJMGP6mD7PMTysN3Lx1Afgvswpks" // your Bot Token (Get from Botfather)
+const char *ssid = "NOVACI_WIFI"; //Název Wi-fi
+const char *password = "K@TK@&F1L1P&KUB1K"; //heslo na Wi-fi
 
-// Use @myidbot to find out the chat ID of an individual or a group
-// Also note that you need to click "start" on a bot before it can
-// message you
-#define CHAT_ID "2110767400"
+// Nastavení pro bota
+#define BOTtoken "2107324741:AAELdq0KJMGP6mD7PMTysN3Lx1Afgvswpks" //Telegram ID mojeho bota
+
+// Přes @myidbot na Telegramu jsem vyhledal svoje Telegram ID
+#define CHAT_ID "2110767400" //Zapsání mého telegram ID (popřípadě od jiného uživatele) pro bota k rozeznání
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-// Checks for new messages every 1 second.
+//Kontroluje pro nové zprávy každou vteřinu.
 int botRequestDelay = 1000;
 unsigned long lastTimeBotRan;
 
 const int ledPin = 2;
 bool ledState = LOW;
 
-// Handle what happens when you receive new messages
+// Pracuje s novými zprávami.
 void handleNewMessages(int numNewMessages)
 {
   Serial.println("handleNewMessages");
@@ -34,7 +32,7 @@ void handleNewMessages(int numNewMessages)
 
   for (int i = 0; i < numNewMessages; i++)
   {
-    // Chat id of the requester
+    // Kontroluje správnost uživatelského ID
     String chat_id = String(bot.messages[i].chat_id);
     if (chat_id != CHAT_ID)
     {
@@ -42,7 +40,7 @@ void handleNewMessages(int numNewMessages)
       continue;
     }
 
-    // Print the received message
+    // Vypíše poslanou zprávu
     String text = bot.messages[i].text;
     Serial.println(text);
 
@@ -50,24 +48,24 @@ void handleNewMessages(int numNewMessages)
 
     if (text == "/start")
     {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/led_on to turn GPIO ON \n";
-      welcome += "/led_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
+      String welcome = "Ahoj, " + from_name + "e.\n";
+      welcome += "Použij následující příkazy pro ovládání bota\n\n";
+      welcome += "/led_on pro zapnutí LEDky \n";
+      welcome += "/led_off pro vypnutí LEDky \n";
+      welcome += "/state pro momentální stav LEDky \n";
       bot.sendMessage(chat_id, welcome, "");
     }
 
     if (text == "/led_on")
     {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
+      bot.sendMessage(chat_id, "Zapínám LEDku", "");
       ledState = HIGH;
       digitalWrite(ledPin, ledState);
     }
 
     if (text == "/led_off")
     {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
+      bot.sendMessage(chat_id, "Vypínám LEDku", "");
       ledState = LOW;
       digitalWrite(ledPin, ledState);
     }
@@ -76,11 +74,11 @@ void handleNewMessages(int numNewMessages)
     {
       if (digitalRead(ledPin))
       {
-        bot.sendMessage(chat_id, "LED is ON", "");
+        bot.sendMessage(chat_id, "LEDka je zapnutá", "");
       }
       else
       {
-        bot.sendMessage(chat_id, "LED is OFF", "");
+        bot.sendMessage(chat_id, "LED je vypnutá", "");
       }
     }
   }
@@ -93,72 +91,39 @@ void setup()
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, ledState);
 
-  // Connect to Wi-Fi
+  // Připojování k Wi-fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 #ifdef ESP32
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Přidání kořenového certifikátu pro api.telegram.org
 #endif
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Připojování se k WiFi..");
   }
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
 
   pinMode(Hall_sensor, INPUT);
-  /*while(Val1=2)
-  {
-    Val1 = digitalRead(Hall_sensor); //We read both values and display them raw on the serial monitor
-    Serial.print(Val1);
-    Serial.print("\t");
   }
-  delay (1000);*/
-}
-
-/*void Hall_Sensor()
-{
-  Val1 = analogRead(Hall_sensor); //We read both values and display them raw on the serial monitor
-  Serial.print(Val1);
-  Serial.print("\t");
-  delay (1000);
-}*/
 
 void loop()
 {
   if (millis() > lastTimeBotRan + botRequestDelay)
   {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-   
-    while (numNewMessages)
-    {
+    
+    while (numNewMessages) 
+    { 
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
     lastTimeBotRan = millis();
   }
-  Val1=digitalRead(Hall_sensor);            //We read both values and display them raw on the serial monitor
+  Val1=digitalRead(Hall_sensor);            //Čtení a zapsání hodnoty Z Hallova sensoru
    Serial.print(Val1);
    Serial.print("\t");
    delay(1000);
 }
-#include <ArduinoJson.h>
-#define LED 34
-
-
-/*void setup () {
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
-  
-}
-
-void loop () {
-  
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
-  
-}*/
