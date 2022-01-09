3844dd60e6e130d106c74f270401216ac1cb7ebc
Kuba <jakub.kuba.novak@gmail.com>
2022-01-09 18:43:53 +0100

    změny v kódu

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <UniversalTelegramBot.h> // Knihovna pro Telegram bota od Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 #include <ArduinoJson.h>
 #define Hall_sensor 35
+#define Hall_sensor2 34
 int Val1 = 0;
+int Val2= 0;
 
 const char *wifi_jmeno = "NOVACI_WIFI";  //Název Wi-fi
 const char *heslo = "K@TK@&F1L1P&KUB1K"; //heslo na Wi-fi
@@ -48,13 +50,13 @@ void handleNewMessages(int numNewMessages)
 
     if (text == "/start") //bot při zprávě /start pošle následující zprávu
     {
-      String welcome = "Ahoj, " + from_name + "e.\n";
+      String welcome = "Ahoj, " + from_name + ".\n";
       welcome += "Použij následující příkazy pro ovládání bota\n";
-      welcome += "/state pro momentální stav schránky.\n";
+      welcome += "/stav pro momentální stav schránky.\n";
       bot.sendMessage(chat_id, welcome, "");
     }
 
-    if (text == "/state") //bot podle hodnoty Hallova senzoru pošle zprávu o stavu schránky
+    if (text == "/stav") //bot podle hodnoty Hallova senzoru pošle zprávu o stavu schránky
     {
       if (digitalRead(Val1))
       {
@@ -90,13 +92,24 @@ void setup()
   Serial.println(WiFi.localIP());
 
   pinMode(Hall_sensor, INPUT);
+  pinMode(Hall_sensor2, INPUT);
 }
-void zprava(int value)
+void postaIn(int value)
 {
   String chat_id = CHAT_ID;
   if (value == 0)
   {
-    bot.sendMessage(chat_id, "Schránka se zavřela", "");
+    bot.sendMessage(chat_id, "Do schránky byla vložena pošta.", "");
+    delay(5000);
+  }
+}
+
+void postaOut(int value)
+{
+  String chat_id = CHAT_ID;
+  if (value == 0)
+  {
+    bot.sendMessage(chat_id, "Schránka byla vybrána.", "");
     delay(5000);
   }
 }
@@ -118,9 +131,15 @@ void loop()
   }
 
   Val1 = digitalRead(Hall_sensor); //Čtení a zapsání hodnoty Z Hallova sensoru
+  Val2 = digitalRead(Hall_sensor2);
   Serial.print(Val1);
+  Serial.print("\t");
+  Serial.print(Val2);
   Serial.print("\t");
   delay(1000);
 
-  zprava(Val1);
+  postaIn(Val1);
+  postaOut(Val2);
+
+
 }
